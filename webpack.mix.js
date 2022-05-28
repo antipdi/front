4a9056ff98565c13e537e8f0502f4027b0c3c803
antipdi <danil.antipov090401@mail.ru>
2022-05-28 12:11:36 +1000

    HW_5

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,16 +1,18 @@
 const mix = require('laravel-mix');
+const { resolve } = require('path');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
- | file for the application as well as bundling up all the JS files.
- |
- */
+mix
+    .js('resources/scripts/main.js', 'build/bundle.js')
+    .sass('resources/styles/main.scss', 'build/bundle.css', {}, [
+        require('postcss-axis')
+    ])
+    .browserSync({
+        proxy: 'localhost',
+        files: [
+            'public/**/*',
+            'resources/**/*.twig',
+            'resources/**/*.php'
+        ]
+    });
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/css/app.scss', 'public/css')
-    .browserSync('localhost');
+mix.alias({'uikit-util': resolve(__dirname, 'node_modules/uikit/src/js/util')});
